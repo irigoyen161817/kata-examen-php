3d6dbe8cdd6b7f2d135b2d0a3a207be10b73f6f5
Alejandro Irigoyen <irigoyen.161817@e.unavarra.es>
2026-05-28 16:27:45 +0200

    [verde] añadir código para tener en cuenta cantidad de comida

--- src/Command.php
+++ src/Command.php
@@ -6,6 +6,10 @@ class Command
 {
     public function handle($instruction): string{
         $instruction = explode(" ", $instruction);
+
+        if(isset($instruction[2])){
+            return $instruction[1] . " x$instruction[2]";
+        }
         return $instruction[1];
     }
 }
